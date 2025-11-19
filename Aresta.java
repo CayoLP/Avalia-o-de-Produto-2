ca6934cf681781e469e32d7ad5777e7f89358085
Erick Nogueira Alvarenga <117676267+ErickNogueira16@users.noreply.github.com>
2025-11-18 22:40:10 -0300

    Add files via upload

---
+++ Aresta.java
@@ -0,0 +1,11 @@
+
+public class Aresta {
+    public int origem, destino;
+    public double peso;
+
+    public Aresta(int origem, int destino, double peso){
+        this.origem = origem;
+        this.destino = destino;
+        this.peso = peso;
+    }
+}
